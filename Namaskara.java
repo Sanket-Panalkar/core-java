bf62bc53c853f91a50efad09d2be10ae9fb8f8dc
Sanket Panalkar <sanketp.xworkz@gmail.com>
2022-11-23 20:34:03 +0530

    committing java files

---
+++ Namaskara.java
@@ -0,0 +1,6 @@
+class Namaskara{
+  
+  public static void main(String a[]){
+    System.out.println("Banni Java Kaliyona");
+  }
+}
---
+++ Transport.java
@@ -0,0 +1,8 @@
+class Transport{
+
+ public static void main(String arg[]){
+ 
+    System.out.println("diffrent mean of transport is ---- Metro, bus, vehicals(2 wheeler and 4 wheeler)");
+ }
+
+}
